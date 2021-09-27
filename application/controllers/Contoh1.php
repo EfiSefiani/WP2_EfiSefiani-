f82056e2be6a9dd839063664983222d403370ed2
efisefiani <efisefiani28@gmail.com>
2021-09-27 23:23:07 +0700

    u

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+    public function index()
+        {
+            echo "Perkenalkan<br>";
+            echo "Nama Saya <b>Efi Sefiani</b>";    
+            echo "Dengan Nim <b>19200237</b>";  
+        }
+}
